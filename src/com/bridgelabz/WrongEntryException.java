681f6e24f9faa557dc5a95ca7de4d29ad80d781b
kolhechetna <kolhe.chetna@gmail.com>
2022-12-28 17:08:32 +0530

    Added:throw custom exceptions in case of Invalid User Details.

---
+++ src/com/bridgelabz/WrongEntryException.java
@@ -0,0 +1,50 @@
+package com.bridgelabz;
+
+import java.util.regex.Pattern;
+
+public class WrongEntryException extends Exception {
+	WrongEntryException(String message){
+		super(message);
+		
+	}
+
+}
+class UserEntryDetails1   {
+	public static void checkValidfirstname(String fName) {
+		boolean result=Pattern.matches(("[A-Z][a-z]{3,}"),fName);
+		try {
+			if(result==false) {
+				throw new WrongEntryException("Wrong eNTRY");
+			}
+			else {
+				System.out.println("continue");
+			}
+			}
+			catch(WrongEntryException w) {
+				w.printStackTrace();
+			}
+			System.out.println("Exception not required ");
+
+	}
+	public static void checkValidphonenumber(String phoneNumber) {
+	boolean result=Pattern.matches(("[1-9]{2}\s[0-9]{10}"),phoneNumber);
+	try {
+	if(result==false) {
+		throw new WrongEntryException("Wrong eNTRY");
+	}
+	else {
+		System.out.println("continue");
+	}
+	}
+	catch(WrongEntryException w) {
+		w.printStackTrace();
+	}
+	System.out.println("Exception excecuted");
+
+}
+	public static void main(String[] args) {
+		//checkValidphonenumber("12768728");
+		//System.out.println("hello");
+		checkValidfirstname("Finny");
+	}
+	}
